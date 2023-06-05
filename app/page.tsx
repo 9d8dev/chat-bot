aa49698dbfd6cf519569397dbc1f945cdf8798f6
Bridger Tower <bridgertower@gmail.com>
2023-06-05 14:22:22 -0600

    Updated

--- app/page.tsx
+++ app/page.tsx
@@ -5,6 +5,7 @@ function Home() {
 	return (
 		<main className="flex md:mt-12 flex-col items-center w-full">
 			<Header />
+			<a className='sw' href="https://github.com/brijr/chat-bot">See the Repo on GitHub</a>
 			<a
 				className="mb-8 block"
 				href="https://vercel.com/new/clone?repository-url=https%3A%2F%2Fgithub.com%2Fbrijr%2Fchat-bot&env=OPENAI_API_KEY,OPENAI_ORGANIZATION&envDescription=API%20Keys%20needed%20for%20Chat%20Bot%20to%20work.&envLink=https%3A%2F%2Fplatform.openai.com%2F"
