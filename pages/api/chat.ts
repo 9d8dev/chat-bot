379c8981a073f03fa302a0c6d4890bfd6f9c510f
Bridger Tower <bridgertower@gmail.com>
2023-06-05 10:48:40 -0600

    First Commit

---
+++ pages/api/chat.ts
@@ -0,0 +1,52 @@
+import { type ChatGPTMessage } from "@/components/ai-chat/ChatLine";
+import { OpenAIStream, OpenAIStreamPayload } from "@/utils/OpenAIStream";
+
+// break the app if the API key is missing
+if (!process.env.OPENAI_API_KEY) {
+  throw new Error("Missing Environment Variable OPENAI_API_KEY");
+}
+
+export const config = {
+  runtime: "edge",
+};
+
+const handler = async (req: Request): Promise<Response> => {
+  const body = await req.json();
+
+  const messages: ChatGPTMessage[] = [
+    {
+      role: "system",
+      content:
+        "An AI assistant that is an expert in marketing, sales, and customer support. An AI Assistant that is kind and helpful. An AI assistant that is very knowledgeable and confident. You work for a company called Ampry that is a Customer Acquisition Marketing company.",
+    },
+  ];
+  messages.push(...body?.messages);
+
+  const requestHeaders: Record<string, string> = {
+    "Content-Type": "application/json",
+    Authorization: `Bearer ${process.env.OPENAI_API_KEY}`,
+  };
+
+  if (process.env.OPENAI_API_ORG) {
+    requestHeaders["OpenAI-Organization"] = process.env.OPENAI_API_ORG;
+  }
+
+  const payload: OpenAIStreamPayload = {
+    model: "gpt-3.5-turbo",
+    messages: messages,
+    temperature: process.env.AI_TEMP ? parseFloat(process.env.AI_TEMP) : 0.8,
+    max_tokens: process.env.AI_MAX_TOKENS
+      ? parseInt(process.env.AI_MAX_TOKENS)
+      : 2048,
+    top_p: 1,
+    frequency_penalty: 0,
+    presence_penalty: 0,
+    stream: true,
+    user: body?.user,
+    n: 1,
+  };
+
+  const stream = await OpenAIStream(payload);
+  return new Response(stream);
+};
+export default handler;
